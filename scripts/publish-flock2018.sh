40b8b33f7aa7dccfb01d4b796b0575df6dd01662
Leapp CI Builder <leapp-ci-builder@leapp-to.github.io>
2018-07-30 16:39:39 +0200

    Fix repo spec

--- scripts/publish-flock2018.sh
+++ scripts/publish-flock2018.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-copr-cli build -r fedora-28-x86_64 fedora-rawhide-x86_64 evilissimo/leapp-flock2018 $@
+copr-cli build -r fedora-28-x86_64,fedora-rawhide-x86_64 evilissimo/leapp-flock2018 $@
 RESULT=$?
 
 exit $RESULT
